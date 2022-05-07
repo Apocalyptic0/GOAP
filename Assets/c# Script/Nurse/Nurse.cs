ee98baa1c6a612d7b93d0aa00cb564a209707e17
Apocalyptic0 <101310682+Apocalyptic0@users.noreply.github.com>
2022-05-07 18:56:36 +0800

    first

---
+++ Assets/c# Script/Nurse/Nurse.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nurse : GAgent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        SubGoal s1 = new SubGoal("treatPatient", 1, false);
+        goals.Add(s1, 3);
+        SubGoal s2 = new SubGoal("rested", 1, false);
+        goals.Add(s2, 1);
+
+        Invoke("GetTired", Random.Range(20, 30));
+    }
+
+    void GetTired()
+    {
+        beliefs.ModifyState("exhausted", 1);
+        Invoke("GetTired", Random.Range(10, 20));
+    }
+}
